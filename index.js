fccc037df316a38b3e82e1c65e0a5552830c4b9d
Lee <you@example.com>
2024-03-27 18:15:35 +0300

    The game

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum +1))+minNum;
+
+
+let attempts = 0;
+let guess;
+let running = true;
+
+
+while(running){
+    guess = window.prompt(`Guess a Number between ${minNum} and ${maxNum}`);
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+
+        window.alert("Please enter a valid Number")
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a valid Number")
+
+    }
+    else{
+        attempts++;
+        if(guess < answer){
+            window.alert("TOO LOW! TRY AGAIN!");
+            }
+        else if(guess > answer){
+            window.alert("TOO HIGH! TRY AGAIN!"); 
+        }
+        else{
+            window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
+        running = false;
+        }
+    
+}
+}
